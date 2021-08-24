665a358745016f83f321f3312cd2db3d4d0111c3
Constanzal <conii.lopez@gmail.com>
2021-08-24 03:04:35 +0000

    arreglo

--- trafficlight/src/App.js
+++ trafficlight/src/App.js
@@ -10,9 +10,9 @@ const App = () => {
     <div className="traffic-light">
       <div className="palito"></div>
       <div className="cuerpo">
-        <div className="luz roja" style={{background: "red", boxShadow: shadow1}} onClick={()=> {setShadow1("0 0 100px 50px rgb(236, 84, 79)")}}></div>
-        <div className="luz amarilla" style={{background: "yellow", boxShadow: shadow2}} onClick={()=>{setShadow2("0 0 100px 50px rgb(236, 212, 79)")}}></div>
-        <div className="luz verde" style={{background: "green", boxShadow: shadow3}} onClick={()=>{setShadow3("0 0 100px 50px rgb(79, 236, 113)")}}></div>
+        <div className="luz roja" style={{boxShadow: shadow1}} onClick={()=> {setShadow1("0 0 100px 50px rgb(236, 84, 79)")}}></div>
+        <div className="luz amarilla" style={{boxShadow: shadow2}} onClick={()=>{setShadow2("0 0 100px 50px rgb(236, 212, 79)")}}></div>
+        <div className="luz verde" style={{boxShadow: shadow3}} onClick={()=>{setShadow3("0 0 100px 50px rgb(79, 236, 113)")}}></div>
       </div>
     </div>
   );
